8519680cb3ccd2246e4e5564b22492d288a53f4b
Josh Carp <jm.carp@gmail.com>
2025-12-31 18:40:21 +0000

    Fix naming conflict with illumos's regset.h.
    Illumos defines a `DS` macro in its regset.h:
    
    https://github.com/illumos/illumos-gate/blob/master/usr/src/uts/intel/sys/regset.h#L111
    
    This conflicts with the DS template parameters used in structs in
    boost/geometry/strategies/line_interpolate. This patch renames the
    conflicting DS parameters to avoid the conflict.

--- boost/geometry/strategies/line_interpolate/cartesian.hpp
+++ boost/geometry/strategies/line_interpolate/cartesian.hpp
@@ -56,10 +56,10 @@ struct default_strategy<Geometry, cartesian_tag>
 };
 
 
-template <typename CT, typename DS>
-struct strategy_converter<strategy::line_interpolate::cartesian<CT, DS> >
+template <typename CT, typename DST>
+struct strategy_converter<strategy::line_interpolate::cartesian<CT, DST> >
 {
-    static auto get(strategy::line_interpolate::cartesian<CT, DS> const&)
+    static auto get(strategy::line_interpolate::cartesian<CT, DST> const&)
     {
         return strategies::line_interpolate::cartesian<CT>();
     }
